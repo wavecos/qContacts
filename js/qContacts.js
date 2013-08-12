a461915f0dfd034299652ab38ad899702be4e77e
Alfredo Arias <alfre.arias@gmail.com>
2013-08-12 12:18:40 -0400

    changes in ui

--- js/qContacts.js
+++ js/qContacts.js
@@ -3,8 +3,29 @@ var qContactsModule = angular.module('qContacts', []);
 /* Controllers */
 qContactsModule.controller('ContactsListController', function($scope, $http) {
 
-	var url = 'dummy_contacts.json';
+	// var url = 'dummy_contacts.json';
 
+	var contacts = [
+				{
+					"name": "Coca - Cola",
+					"representant": "Juan Perez",
+					"phone_number": "4487215"
+				},
+				{
+					"name": "Coca - Cola",
+					"representant": "Juan Perez",
+					"phone_number": "4487215"
+				},
+				{
+					"name": "Coca - Cola",
+					"representant": "Juan Perez",
+					"phone_number": "4487215"
+				}
+	];
+
+	$scope.contacts = contacts;
+
+	/*
 	$http.get(url).success(function(data, status, headers, config) {
 		var lista = JSON.parse(data);
 		$scope.contacts = lista;
@@ -13,6 +34,7 @@ qContactsModule.controller('ContactsListController', function($scope, $http) {
 	error(function(error) {
 		console.log('error :: ' + error);
 	});
+	*/
 
 });
 
